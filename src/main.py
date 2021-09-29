2b316e7531d29372817cae4fb7bce0855768aa65
HetDaftary <hetdaftary@gmail.com>
2021-09-29 17:02:43 +0530

    Adding theme feature

--- src/main.py
+++ src/main.py
@@ -12,6 +12,11 @@ from PyQt5.QtWidgets import (
 )
 from src.AdditionalFunctions.GetScreenSize import getScreenSize
 import sys
+from darkdetect import isDark
+from qt_material import apply_stylesheet
+
+darkTheme, lightTheme = 'dark_blue.xml', 'light_blue.xml'
+styleNames = ["OS preferred", "light Theme", "dark theme"]
 
 # Deciding app size has per screen size.
 size = [0, 0]
@@ -28,13 +33,16 @@ else:
     size[1] = screenSize[1]
 
 class MainWindow(QMainWindow):
-    def __init__(self) -> None:
+    def __init__(self, app) -> None:
         super().__init__()
+        self.app = app
 
         self.historyArray = []
-
+        
         self.initUI()
         self.initMenu()
+
+        self.changeStyle(self.osThemeItem)
 
         self.show()
 
@@ -54,27 +62,42 @@ class MainWindow(QMainWindow):
         self.advancedCalculatorWidget.hide()
 
     def switchToNormalModeFunction(self):
-        '''
+        
         self.simpleCalculatorWidget.lineEdit.setText(    
             self.advancedCalculatorWidget.lineEdit.text()
         )
-        '''
+        
         self.advancedCalculatorWidget.hide()
         self.simpleCalculatorWidget.show()
     
     def switchToAdvancedModeFunction(self):
-        '''
+        
         self.advancedCalculatorWidget.lineEdit.setText(
             self.simpleCalculatorWidget.lineEdit.text()
         )
-        '''
+        
         self.simpleCalculatorWidget.hide()
         self.advancedCalculatorWidget.show()
+
+    def changeStyle(self, menuAction):
+        global darkTheme, lightTheme
+        global styleNames
+
+        if menuAction == self.lightThemeItem:  
+            apply_stylesheet(self, lightTheme)
+        elif menuAction == self.darkThemeItem:
+            apply_stylesheet(self, darkTheme)
+        else:
+            if isDark():
+                apply_stylesheet(self, darkTheme)
+            else:
+                apply_stylesheet(self, lightTheme)
 
     def initMenu(self):
         self.menuBar = QMenuBar()
         self.setMenuBar(self.menuBar)
 
+        # Switch mode menu. Start.
         self.switchModeMenu = QMenu("Switch Mode")
         self.menuBar.addMenu(self.switchModeMenu)
 
@@ -85,6 +108,25 @@ class MainWindow(QMainWindow):
         self.advancedModeItem = QAction("Advanced Mode")
         self.switchModeMenu.addAction(self.advancedModeItem)
         self.advancedModeItem.triggered.connect(self.switchToAdvancedModeFunction)
+        # Switch mode menu. End.
+
+        # Theme menu. Start.
+        self.themeMenu = QMenu("Theme")
+        self.menuBar.addMenu(self.themeMenu)
+
+        global styleNames
+
+        self.osThemeItem = QAction(styleNames[0])
+        self.themeMenu.addAction(self.osThemeItem)
+        self.osThemeItem.triggered.connect(lambda: self.changeStyle(self.osThemeItem))
+
+        self.darkThemeItem = QAction(styleNames[1])
+        self.themeMenu.addAction(self.darkThemeItem)
+        self.darkThemeItem.triggered.connect(lambda: self.changeStyle(self.lightThemeItem))
+
+        self.lightThemeItem = QAction(styleNames[2])
+        self.themeMenu.addAction(self.lightThemeItem)
+        self.lightThemeItem.triggered.connect(lambda: self.changeStyle(self.darkThemeItem))
 
 class CalculatorApp(QApplication):
     def __init__(self) -> None:
@@ -93,7 +135,7 @@ class CalculatorApp(QApplication):
         sys.exit(self.exec_())
 
     def initUI(self):
-        self.mainWindow = MainWindow()
+        self.mainWindow = MainWindow(self)
 
 if __name__ == "__main__":
     app = CalculatorApp()
